8cf5e247451cc9d44e86905931e701055a45c492
Simone <startae14@gmail.com>
2022-02-01 16:25:10 +0100

    03: Deno logging

--- mod.ts
+++ mod.ts
@@ -1,9 +1,30 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+
+await log.setup({
+    handlers: {
+      console: new log.handlers.ConsoleHandler("DEBUG")
+    },
+    loggers: {
+        // configure default logger available via short-hand methods above.
+        default: {
+          level: "WARNING",
+          handlers: ["console"],
+        },
+    },
+});
+
 async function downloadLaunchData() {
+    log.info("Downloading launch data...");
+    log.warning("THIS IS A WARNING");
     const response = await fetch("https://api.spacexdata.com/v4/launches", {
         method: "GET"
     });
+    if (!response.ok) {
+        log.warning("Problem downloading launch data.");
+        throw new Error("Launch data download failed.");
+    }
     const launchData = await response.json();
-    console.log("launchData", launchData);
+    //console.log("launchData", launchData);
 }
 
 downloadLaunchData();
