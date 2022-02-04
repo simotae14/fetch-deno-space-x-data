5dc29f35b8b9a7448d21e5364e7c7f8edd78f7f8
Simone <startae14@gmail.com>
2022-02-04 18:05:05 +0100

    05: use the Map data structure

--- mod.ts
+++ mod.ts
@@ -1,22 +1,16 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 
-await log.setup({
-    handlers: {
-      console: new log.handlers.ConsoleHandler("DEBUG")
-    },
-    loggers: {
-        // configure default logger available via short-hand methods above.
-        default: {
-          level: "WARNING",
-          handlers: ["console"],
-        },
-    },
-});
+// define a Map
+interface Launch {
+    flightNumber: number;
+    mission: string;
+}
+const launches = new Map<number, Launch>();
 
 async function downloadLaunchData() {
     log.info("Downloading launch data...");
     log.warning("THIS IS A WARNING");
-    const response = await fetch("https://api.spacexdata.com/v4/launches", {
+    const response = await fetch("https://api.spacexdata.com/v3/launches", {
         method: "GET"
     });
     if (!response.ok) {
@@ -24,7 +18,14 @@ async function downloadLaunchData() {
         throw new Error("Launch data download failed.");
     }
     const launchData = await response.json();
-    //console.log("launchData", launchData);
+    for (const launch of launchData) {
+        const flightData = {
+            flightNumber: launch["flight_number"],
+            mission: launch["mission_name"]
+        };
+        launches.set(flightData.flightNumber, flightData);
+        log.info(JSON.stringify(flightData));
+    }
 }
 
 downloadLaunchData();
