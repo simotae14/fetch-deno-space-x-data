1894b58212b25644fe257cf7feb4cf6f0e55eae9
Simone <startae14@gmail.com>
2022-02-10 15:28:22 +0100

    use Lodash flatMap to retrieve some nested properties inside an array

--- mod.ts
+++ mod.ts
@@ -1,9 +1,13 @@
 import * as log from "https://deno.land/std/log/mod.ts";
+// import lodash
+import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/lodash.js";
 
 // define a Map
 interface Launch {
     flightNumber: number;
     mission: string;
+    rocket: string;
+    customers: Array<string>;
 }
 const launches = new Map<number, Launch>();
 
@@ -19,9 +23,13 @@ async function downloadLaunchData() {
     }
     const launchData = await response.json();
     for (const launch of launchData) {
+        const payloads = launch["rocket"]["second_stage"]["payloads"];
+        const customers = _.flatMap(payloads, (payload : any) => payload["customers"]);
         const flightData = {
             flightNumber: launch["flight_number"],
-            mission: launch["mission_name"]
+            mission: launch["mission_name"],
+            rocket: launch["rocket"]["rocket_name"],
+            customers
         };
         launches.set(flightData.flightNumber, flightData);
         log.info(JSON.stringify(flightData));
